7c63296558330c98e3d4d8b14c531b1304fae007
Rafif Athallah <rafifathallah99@gmail.com>
2024-03-26 22:07:38 +0700

    Refactor: Update popup function

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,24 @@
-// Fugsi buka dan tutup modal/dialog
-function openModal(data) {
-    const modal = document.getElementById(data);
-    modal.classList.remove('hidden');
-    setTimeout(() => {
-        modal.classList.add('opacity-100');
-    }, 80);
+// Fugsi buka dan tutup popup
+function togglePopup(data) {
+    var popup = document.getElementById(data);
+    if (popup.classList.contains('hidden')) {
+        // Jika popup tersembunyi, tampilkan
+        popup.classList.remove('hidden');
+        popup.classList.add('flex', 'items-center', 'justify-center');
+        setTimeout(() => {
+            popup.classList.add('opacity-100');
+        }, 80);
+    } else {
+        // Jika popup ditampilkan, sembunyikan
+        popup.classList.remove('flex', 'items-center', 'justify-center');
+        popup.classList.add('hidden');
+        setTimeout(() => {
+            popup.classList.remove('opacity-100');
+        }, 80);
+    }
 }
 
-function closeModal(data) {
-    const modal = document.getElementById(data);
-    modal.classList.add('hidden');
-    setTimeout(() => {
-        modal.classList.remove('opacity-100');
-    }, 80);
-}
-
-function switchModal(data1, data2) {
-    closeModal(data1);
-    openModal(data2);
+function switchPopup(data1, data2) {
+    togglePopup(data1);
+    togglePopup(data2);
 }

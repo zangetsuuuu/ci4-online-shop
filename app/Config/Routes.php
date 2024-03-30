3b858833fbcfce6df64207594e168632f0ff4b5f
Rafif Athallah <rafifathallah99@gmail.com>
2024-03-30 18:46:47 +0700

    Update: Add new routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/home', 'Page::index');
+$routes->get('/cart', 'Page::cart');
+$routes->get('/account', 'Page::account');
+
+$routes->get('/register', 'User::register');
+$routes->get('/login', 'User::login');

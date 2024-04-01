014e39d4357375df11e2a003bbf0d05fb652e454
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-01 14:59:00 +0700

    Update: Add new routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,7 +9,9 @@ $routes->get('/', 'Home::index');
 
 $routes->get('/home', 'Page::index');
 $routes->get('/cart', 'Page::cart');
+$routes->get('/transaction', 'Page::transaction');
 $routes->get('/account', 'Page::account');
+$routes->get('/about-us', 'Page::about');
 
 $routes->get('/register', 'User::register');
 $routes->get('/login', 'User::login');

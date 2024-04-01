2c414adfd683cae5fab5e3d148f338b62b4fc711
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-01 15:15:56 +0700

    Style: Minor changes

--- app/Views/index.php
+++ app/Views/index.php
@@ -4,8 +4,8 @@
 <div class="min-h-screen flex items-center justify-center">
     <div class="container px-4 py-16 md:px-24">
         <div class="md:text-center max-w-4xl mx-auto">
-            <h1 class="text-3xl md:text-4xl font-bold tracking-wide mb-3 md:mb-4">Selamat Datang di OnlineStore!</h1>
-            <p class="text-sm md:text-base text-gray-500 tracking-wide mb-6">Cari dan beli barang yang Anda mau hanya dari rumah, disini!</p>
+            <h1 class="text-4xl md:text-5xl font-bold tracking-wide mb-3 md:mb-4">Selamat Datang di OnlineStore!</h1>
+            <p class="text-base md:text-lg text-gray-500 tracking-wide mb-6">Cari dan beli barang yang Anda mau hanya dari rumah, disini!</p>
             <div class="flex flex-wrap items-center justify-center space-x-0 md:space-x-3 space-y-3 md:space-y-0">
                 <a href="/register" class="btn-primary w-full md:w-auto">
                     Daftar

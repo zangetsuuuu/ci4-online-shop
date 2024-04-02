809b62a5af33444ad7a568303af74a3f6dcf4637
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-02 22:56:48 +0700

    Style: Add carousel and product card

--- app/Views/pages/users/home.php
+++ app/Views/pages/users/home.php
@@ -4,8 +4,147 @@
 <div class="min-h-screen">
     <div class="p-4 sm:ml-64">
         <div class="h-full bg-white rounded-lg p-4 mt-14">
-            <h1>tes</h1>
+            <!-- Carousel Start -->
+            <div id="controls-carousel" class="relative w-full" data-carousel="slide">
+                <div class="relative border overflow-hidden rounded-lg h-[10rem] md:h-80">
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="https://onlineprint.co.id/blog/wp-content/uploads/2023/04/banner-food-800x445.jpg" class="absolute w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover" alt="...">
+                    </div>
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="img/gradient.jpg" class="absolute w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover" alt="...">
+                    </div>
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="img/bg-1.jpg" class="absolute w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover" alt="...">
+                    </div>
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="img/gradient.jpg" class="absolute w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover" alt="...">
+                    </div>
+                </div>
+                <!-- Slider controls -->
+                <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-myBlack/20 group-hover:bg-white/30 group-focus:ring-4 group-focus:ring-white group-focus:outline-none ease-in-out duration-300">
+                        <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                        </svg>
+                        <span class="sr-only">Previous</span>
+                    </span>
+                </button>
+                <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-myBlack/20 group-hover:bg-white/30 group-focus:ring-4 group-focus:ring-white group-focus:outline-none ease-in-out duration-300">
+                        <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                        </svg>
+                        <span class="sr-only">Next</span>
+                    </span>
+                </button>
+            </div>
+            <!-- Carousel End -->
+
+            <!-- Product Start -->
+            <div class="grid grid-cols-2 md:grid-cols-4 gap-5 mt-6">
+                <a href="/product/tes" class="max-h-[40rem] bg-white border border-gray-200 rounded-lg overflow-hidden shadow-sm hover:scale-105 ease-in-out duration-300">
+                    <form action="" method="l6">
+                        <div class="h-32 md:h-40">
+                            <img class="w-full h-full object-contain" src="https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//93/MTA-2583228/indomie_indomie-goreng-mie-instan--85g--_full02.jpg" alt="" />
+                        </div>
+                        <div class="p-3 md:p-4">
+                            <div class="space-y-1 mb-4">
+                                <h5 class="text-base md:text-lg font-semibold tracking-wide text-myBlack truncate">Indomie Goreng</h5>
+                                <p class="text-xs md:text-sm tracking-wide text-gray-500">Stok: 43</p>
+                            </div>
+                            <div class="flex items-center justify-between rounded-md">
+                                <h1 class="text-base md:text-lg font-semibold tracking-wide">Rp. 4.500</h1>
+                                <button data-tooltip-target="cart-tooltip#" type="submit" class="px-3 md:px-4 py-2 text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ease-in-out duration-300">
+                                    <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M3.04047 2.29242C2.6497 2.15503 2.22155 2.36044 2.08416 2.7512C1.94678 3.14197 2.15218 3.57012 2.54295 3.7075L2.80416 3.79934C3.47177 4.03406 3.91052 4.18961 4.23336 4.34802C4.53659 4.4968 4.67026 4.61723 4.75832 4.74609C4.84858 4.87818 4.91828 5.0596 4.95761 5.42295C4.99877 5.80316 4.99979 6.29837 4.99979 7.03832L4.99979 9.64C4.99979 12.5816 5.06302 13.5523 5.92943 14.4662C6.79583 15.38 8.19028 15.38 10.9792 15.38H16.2821C17.8431 15.38 18.6236 15.38 19.1753 14.9304C19.727 14.4808 19.8846 13.7164 20.1997 12.1875L20.6995 9.76275C21.0466 8.02369 21.2202 7.15417 20.7762 6.57708C20.3323 6 18.8155 6 17.1305 6H6.49233C6.48564 5.72967 6.47295 5.48373 6.4489 5.26153C6.39517 4.76515 6.27875 4.31243 5.99677 3.89979C5.71259 3.48393 5.33474 3.21759 4.89411 3.00139C4.48203 2.79919 3.95839 2.61511 3.34187 2.39838L3.04047 2.29242ZM13 8.25C13.4142 8.25 13.75 8.58579 13.75 9V10.25H15C15.4142 10.25 15.75 10.5858 15.75 11C15.75 11.4142 15.4142 11.75 15 11.75H13.75V13C13.75 13.4142 13.4142 13.75 13 13.75C12.5858 13.75 12.25 13.4142 12.25 13V11.75H11C10.5858 11.75 10.25 11.4142 10.25 11C10.25 10.5858 10.5858 10.25 11 10.25H12.25V9C12.25 8.58579 12.5858 8.25 13 8.25Z" fill="currentColor" />
+                                        <path d="M7.5 18C8.32843 18 9 18.6716 9 19.5C9 20.3284 8.32843 21 7.5 21C6.67157 21 6 20.3284 6 19.5C6 18.6716 6.67157 18 7.5 18Z" fill="currentColor" />
+                                        <path d="M16.5 18.0001C17.3284 18.0001 18 18.6716 18 19.5001C18 20.3285 17.3284 21.0001 16.5 21.0001C15.6716 21.0001 15 20.3285 15 19.5001C15 18.6716 15.6716 18.0001 16.5 18.0001Z" fill="currentColor" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </a>
+                <div id="cart-tooltip#" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-xs tracking-wide font-medium text-white transition-opacity duration-300 bg-myBlack rounded-lg shadow-sm opacity-0 tooltip">
+                    Tambah ke Keranjang
+                    <div class="tooltip-arrow" data-popper-arrow></div>
+                </div>
+                <a href="/product/tes" class="md:max-h-[40rem] bg-white border border-gray-200 rounded-lg overflow-hidden shadow-sm hover:scale-105 ease-in-out duration-300">
+                    <div class="h-32 md:h-40">
+                        <img class="w-full h-full object-contain" src="https://jumbo.co.id/wp-content/uploads/2020/03/double-choc.png" alt="" />
+                    </div>
+                    <div class="p-3 md:p-4">
+                        <div class="space-y-1 mb-4">
+                            <h5 class="text-base md:text-lg font-semibold tracking-wide text-myBlack truncate">Goodtime Choco Chips</h5>
+                            <p class="text-xs md:text-sm tracking-wide text-gray-500">Stok: 31</p>
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <h1 class="text-base md:text-lg font-semibold tracking-wide">Rp. 9.000</h1>
+                            <button data-tooltip-target="cart-tooltip#" type="submit" class="px-3 md:px-4 py-2 text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ease-in-out duration-300">
+                                <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M3.04047 2.29242C2.6497 2.15503 2.22155 2.36044 2.08416 2.7512C1.94678 3.14197 2.15218 3.57012 2.54295 3.7075L2.80416 3.79934C3.47177 4.03406 3.91052 4.18961 4.23336 4.34802C4.53659 4.4968 4.67026 4.61723 4.75832 4.74609C4.84858 4.87818 4.91828 5.0596 4.95761 5.42295C4.99877 5.80316 4.99979 6.29837 4.99979 7.03832L4.99979 9.64C4.99979 12.5816 5.06302 13.5523 5.92943 14.4662C6.79583 15.38 8.19028 15.38 10.9792 15.38H16.2821C17.8431 15.38 18.6236 15.38 19.1753 14.9304C19.727 14.4808 19.8846 13.7164 20.1997 12.1875L20.6995 9.76275C21.0466 8.02369 21.2202 7.15417 20.7762 6.57708C20.3323 6 18.8155 6 17.1305 6H6.49233C6.48564 5.72967 6.47295 5.48373 6.4489 5.26153C6.39517 4.76515 6.27875 4.31243 5.99677 3.89979C5.71259 3.48393 5.33474 3.21759 4.89411 3.00139C4.48203 2.79919 3.95839 2.61511 3.34187 2.39838L3.04047 2.29242ZM13 8.25C13.4142 8.25 13.75 8.58579 13.75 9V10.25H15C15.4142 10.25 15.75 10.5858 15.75 11C15.75 11.4142 15.4142 11.75 15 11.75H13.75V13C13.75 13.4142 13.4142 13.75 13 13.75C12.5858 13.75 12.25 13.4142 12.25 13V11.75H11C10.5858 11.75 10.25 11.4142 10.25 11C10.25 10.5858 10.5858 10.25 11 10.25H12.25V9C12.25 8.58579 12.5858 8.25 13 8.25Z" fill="currentColor" />
+                                    <path d="M7.5 18C8.32843 18 9 18.6716 9 19.5C9 20.3284 8.32843 21 7.5 21C6.67157 21 6 20.3284 6 19.5C6 18.6716 6.67157 18 7.5 18Z" fill="currentColor" />
+                                    <path d="M16.5 18.0001C17.3284 18.0001 18 18.6716 18 19.5001C18 20.3285 17.3284 21.0001 16.5 21.0001C15.6716 21.0001 15 20.3285 15 19.5001C15 18.6716 15.6716 18.0001 16.5 18.0001Z" fill="currentColor" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </a>
+                <div id="cart-tooltip#" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-xs tracking-wide font-medium text-white transition-opacity duration-300 bg-myBlack rounded-lg shadow-sm opacity-0 tooltip">
+                    Tambah ke Keranjang
+                    <div class="tooltip-arrow" data-popper-arrow></div>
+                </div>
+                <a href="/product/tes" class="md:max-h-[40rem] bg-white border border-gray-200 rounded-lg overflow-hidden shadow-sm hover:scale-105 ease-in-out duration-300">
+                    <div class="h-32 md:h-40">
+                        <img class="w-full h-full object-contain" src="https://www.rinso.com/images/h0nadbhvm6m4/5Kxf41PDRmwY1shdngn99e/b7e6d213048b7ec6940b445691cac402/TGlxdWlkX3Jvc2VfZnJlc2gucG5n/1080w-1080h/rinso-molto-deterjen-cair-rose-fresh-packshot.jpg" alt="" />
+                    </div>
+                    <div class="p-3 md:p-4">
+                        <div class="space-y-1 mb-4">
+                            <h5 class="text-base md:text-lg font-semibold tracking-wide text-myBlack truncate">Rinso Cair</h5>
+                            <p class="text-xs md:text-sm tracking-wide text-gray-500">Stok: 37</p>
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <h1 class="text-base md:text-lg font-semibold tracking-wide">Rp. 20.000</h1>
+                            <button data-tooltip-target="cart-tooltip#" type="submit" class="px-3 md:px-4 py-2 text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ease-in-out duration-300">
+                                <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M3.04047 2.29242C2.6497 2.15503 2.22155 2.36044 2.08416 2.7512C1.94678 3.14197 2.15218 3.57012 2.54295 3.7075L2.80416 3.79934C3.47177 4.03406 3.91052 4.18961 4.23336 4.34802C4.53659 4.4968 4.67026 4.61723 4.75832 4.74609C4.84858 4.87818 4.91828 5.0596 4.95761 5.42295C4.99877 5.80316 4.99979 6.29837 4.99979 7.03832L4.99979 9.64C4.99979 12.5816 5.06302 13.5523 5.92943 14.4662C6.79583 15.38 8.19028 15.38 10.9792 15.38H16.2821C17.8431 15.38 18.6236 15.38 19.1753 14.9304C19.727 14.4808 19.8846 13.7164 20.1997 12.1875L20.6995 9.76275C21.0466 8.02369 21.2202 7.15417 20.7762 6.57708C20.3323 6 18.8155 6 17.1305 6H6.49233C6.48564 5.72967 6.47295 5.48373 6.4489 5.26153C6.39517 4.76515 6.27875 4.31243 5.99677 3.89979C5.71259 3.48393 5.33474 3.21759 4.89411 3.00139C4.48203 2.79919 3.95839 2.61511 3.34187 2.39838L3.04047 2.29242ZM13 8.25C13.4142 8.25 13.75 8.58579 13.75 9V10.25H15C15.4142 10.25 15.75 10.5858 15.75 11C15.75 11.4142 15.4142 11.75 15 11.75H13.75V13C13.75 13.4142 13.4142 13.75 13 13.75C12.5858 13.75 12.25 13.4142 12.25 13V11.75H11C10.5858 11.75 10.25 11.4142 10.25 11C10.25 10.5858 10.5858 10.25 11 10.25H12.25V9C12.25 8.58579 12.5858 8.25 13 8.25Z" fill="currentColor" />
+                                    <path d="M7.5 18C8.32843 18 9 18.6716 9 19.5C9 20.3284 8.32843 21 7.5 21C6.67157 21 6 20.3284 6 19.5C6 18.6716 6.67157 18 7.5 18Z" fill="currentColor" />
+                                    <path d="M16.5 18.0001C17.3284 18.0001 18 18.6716 18 19.5001C18 20.3285 17.3284 21.0001 16.5 21.0001C15.6716 21.0001 15 20.3285 15 19.5001C15 18.6716 15.6716 18.0001 16.5 18.0001Z" fill="currentColor" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </a>
+                <div id="cart-tooltip#" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-xs tracking-wide font-medium text-white transition-opacity duration-300 bg-myBlack rounded-lg shadow-sm opacity-0 tooltip">
+                    Tambah ke Keranjang
+                    <div class="tooltip-arrow" data-popper-arrow></div>
+                </div>
+                <a href="/product/tes" class="md:max-h-[40rem] bg-white border border-gray-200 rounded-lg overflow-hidden shadow-sm hover:scale-105 ease-in-out duration-300">
+                    <div class="h-32 md:h-40">
+                        <img class="w-full h-full object-contain" src="https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//95/MTA-12816437/teh-pucuk_teh-pucuk-jasmine-1-36l_full01.jpg" alt="" />
+                    </div>
+                    <div class="p-3 md:p-4">
+                        <div class="space-y-1 mb-4">
+                            <h5 class="text-base md:text-lg font-semibold tracking-wide text-myBlack truncate">Teh Pucuk</h5>
+                            <p class="text-xs md:text-sm tracking-wide text-gray-500">Stok: 24</p>
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <h1 class="text-base md:text-lg font-semibold tracking-wide">Rp. 6.000</h1>
+                            <button data-tooltip-target="cart-tooltip#" type="submit" class="px-3 md:px-4 py-2 text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ease-in-out duration-300">
+                                <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M3.04047 2.29242C2.6497 2.15503 2.22155 2.36044 2.08416 2.7512C1.94678 3.14197 2.15218 3.57012 2.54295 3.7075L2.80416 3.79934C3.47177 4.03406 3.91052 4.18961 4.23336 4.34802C4.53659 4.4968 4.67026 4.61723 4.75832 4.74609C4.84858 4.87818 4.91828 5.0596 4.95761 5.42295C4.99877 5.80316 4.99979 6.29837 4.99979 7.03832L4.99979 9.64C4.99979 12.5816 5.06302 13.5523 5.92943 14.4662C6.79583 15.38 8.19028 15.38 10.9792 15.38H16.2821C17.8431 15.38 18.6236 15.38 19.1753 14.9304C19.727 14.4808 19.8846 13.7164 20.1997 12.1875L20.6995 9.76275C21.0466 8.02369 21.2202 7.15417 20.7762 6.57708C20.3323 6 18.8155 6 17.1305 6H6.49233C6.48564 5.72967 6.47295 5.48373 6.4489 5.26153C6.39517 4.76515 6.27875 4.31243 5.99677 3.89979C5.71259 3.48393 5.33474 3.21759 4.89411 3.00139C4.48203 2.79919 3.95839 2.61511 3.34187 2.39838L3.04047 2.29242ZM13 8.25C13.4142 8.25 13.75 8.58579 13.75 9V10.25H15C15.4142 10.25 15.75 10.5858 15.75 11C15.75 11.4142 15.4142 11.75 15 11.75H13.75V13C13.75 13.4142 13.4142 13.75 13 13.75C12.5858 13.75 12.25 13.4142 12.25 13V11.75H11C10.5858 11.75 10.25 11.4142 10.25 11C10.25 10.5858 10.5858 10.25 11 10.25H12.25V9C12.25 8.58579 12.5858 8.25 13 8.25Z" fill="currentColor" />
+                                    <path d="M7.5 18C8.32843 18 9 18.6716 9 19.5C9 20.3284 8.32843 21 7.5 21C6.67157 21 6 20.3284 6 19.5C6 18.6716 6.67157 18 7.5 18Z" fill="currentColor" />
+                                    <path d="M16.5 18.0001C17.3284 18.0001 18 18.6716 18 19.5001C18 20.3285 17.3284 21.0001 16.5 21.0001C15.6716 21.0001 15 20.3285 15 19.5001C15 18.6716 15.6716 18.0001 16.5 18.0001Z" fill="currentColor" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </a>
+                <div id="cart-tooltip#" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-xs tracking-wide font-medium text-white transition-opacity duration-300 bg-myBlack rounded-lg shadow-sm opacity-0 tooltip">
+                    Tambah ke Keranjang
+                    <div class="tooltip-arrow" data-popper-arrow></div>
+                </div>
+            </div>
+            <!-- Product Start -->
         </div>
-    </div>
-</div>
-<?= $this->endSection(); ?>
+        <?= $this->endSection(); ?>

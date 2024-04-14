ddfcf1a360e7c8e171d1434b9bc9defc142da091
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-14 07:25:55 +0700

    Update: Add new routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,9 +7,20 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
+$routes->get('/admin', 'Auth::index');
+$routes->get('/register', 'Auth::register');
+$routes->get('/login', 'Auth::login');
+
+$routes->get('/admin/dashboard', 'Page::dashboard');
+$routes->get('/admin/product', 'Page::adminProduct');
+$routes->get('/admin/profile', 'Page::adminProfile');
+$routes->get('/admin/change-password', 'Page::adminChangePassword');
+$routes->get('/admin/orders', 'Page::adminOrders');
+$routes->get('/admin/orders/(:segment)', 'Page::adminOrders/$1');
+$routes->get('/admin/customers', 'Page::customers');
+$routes->get('/admin/customers/(:segment)', 'Page::customerDetails/$1');
+
 $routes->get('/home', 'Page::index');
-$routes->get('/register', 'Page::register');
-$routes->get('/login', 'Page::login');
 $routes->get('/cart', 'Page::cart');
 $routes->get('/orders', 'Page::orders');
 $routes->get('/checkout', 'Page::checkout');
@@ -17,4 +28,5 @@ $routes->get('/about-us', 'Page::about');
 $routes->get('/profile', 'Page::userProfile');
 $routes->get('/change-password', 'Page::changePassword');
 
-$routes->get('/product/(:segment)', 'Page::product/$1');
+$routes->get('/admin/product/(:segment)', 'Page::adminProductDetail/$1');
+$routes->get('/product/(:segment)', 'Page::productDetail/$1');

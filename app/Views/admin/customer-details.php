7680d3e8f06809288ca17f51321c840676410a14
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-14 10:12:04 +0700

    Update: Minor changes

--- app/Views/admin/customer-details.php
+++ app/Views/admin/customer-details.php
@@ -16,17 +16,6 @@
             <div class="grid grid-cols-1 md:grid-cols-3 gap-y-8 md:gap-5">
                 <div class="relative rounded-full w-56 h-56 overflow-hidden border mx-auto">
                     <img src="<?= base_url('img/bg-1.jpg'); ?>" class="object-cover w-full h-full" alt="">
-                    <div class="flex items-center justify-center absolute inset-0 cursor-pointer opacity-0 hover:opacity-100 bg-myBlack/50 ease-in-out duration-300">
-                        <button type="button" data-modal-target="profile-image-modal" data-modal-toggle="profile-image-modal" data-tooltip-target="profile-image-tooltip" class="text-white hover:text-gray-300 p-3 rounded-full ease-in-out duration-300">
-                            <svg class="w-7 h-7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4 9V5.6C4 5.03995 4 4.75992 4.10899 4.54601C4.20487 4.35785 4.35785 4.20487 4.54601 4.109C4.75992 4 5.03995 4 5.6 4L9 4M4 15V18.4C4 18.9601 4 19.2401 4.10899 19.454C4.20487 19.6422 4.35785 19.7951 4.54601 19.891C4.75992 20 5.03995 20 5.6 20L9 20M15 4H18.4C18.9601 4 19.2401 4 19.454 4.10899C19.6422 4.20487 19.7951 4.35785 19.891 4.54601C20 4.75992 20 5.03995 20 5.6V9M20 15V18.4C20 18.9601 20 19.2401 19.891 19.454C19.7951 19.6422 19.6422 19.7951 19.454 19.891C19.2401 20 18.9601 20 18.4 20H15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
-                        </button>
-                        <div id="profile-image-tooltip" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-xs tracking-wide font-medium text-white transition-opacity duration-300 bg-myBlack rounded-lg shadow-sm opacity-0 tooltip group">
-                            Lihat Foto
-                            <div class="tooltip-arrow" data-popper-arrow></div>
-                        </div>
-                    </div>
                 </div>
                 <div class="col-span-2 space-y-6">
                     <div class="space-y-2">

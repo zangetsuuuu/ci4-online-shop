0847eec924aafd7815f0e32306f328e6947740cc
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-16 16:33:54 +0700

    Update: New routes structure

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,28 +5,43 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+// Default route
 $routes->get('/', 'Home::index');
 
-$routes->get('/admin', 'Auth::index');
-$routes->get('/register', 'Auth::register');
-$routes->get('/login', 'Auth::login');
+// Admin routes
+$routes->group('admin', function ($routes) {
+    $routes->get('', 'Auth\Login::admin');
+    $routes->get('change-password', 'Auth\Password::admin');
 
-$routes->get('/admin/dashboard', 'Page::dashboard');
-$routes->get('/admin/product', 'Page::adminProduct');
-$routes->get('/admin/profile', 'Page::adminProfile');
-$routes->get('/admin/change-password', 'Page::adminChangePassword');
-$routes->get('/admin/orders', 'Page::adminOrders');
-$routes->get('/admin/orders/(:segment)', 'Page::adminOrdersDetail/$1');
-$routes->get('/admin/customers', 'Page::customers');
-$routes->get('/admin/customers/(:segment)', 'Page::customerDetails/$1');
+    $routes->get('dashboard', 'Admin\Dashboard::index');
+    $routes->get('products', 'Admin\Products::index');
+    $routes->get('product/create', 'Admin\Products::create');
+    $routes->get('product/(:segment)', 'Admin\Products::detail/$1');
+    $routes->get('product/(:segment)/edit', 'Admin\Products::edit/$1');
+    $routes->get('orders', 'Admin\Orders::index');
+    $routes->get('order/(:segment)', 'Admin\Orders::detail/$1');
+    $routes->get('customers', 'Admin\Customers::index');
+    $routes->get('customer/(:segment)', 'Admin\Customers::detail/$1');
+    $routes->get('profile', 'Admin\Profile::index');
+    $routes->get('profile/(:segment)/edit', 'Admin\Profile::edit/$1');
+});
 
-$routes->get('/home', 'Page::index');
-$routes->get('/cart', 'Page::cart');
-$routes->get('/orders', 'Page::orders');
-$routes->get('/checkout', 'Page::checkout');
-$routes->get('/about-us', 'Page::about');
-$routes->get('/profile', 'Page::userProfile');
-$routes->get('/change-password', 'Page::changePassword');
+// Customer routes
+$routes->group('', function ($routes) {
+    $routes->get('login', 'Auth\Login::customers');
+    $routes->get('register', 'Auth\Register::customers');
+    $routes->get('change-password', 'Auth\Password::customers');
 
-$routes->get('/admin/product/(:segment)', 'Page::adminProductDetail/$1');
-$routes->get('/product/(:segment)', 'Page::productDetail/$1');
+    $routes->get('cart', 'Customers\Cart::index');
+    $routes->get('products', 'Customers\Products::index');
+    $routes->get('product/(:segment)', 'Customers\Products::detail/$1');
+    $routes->get('orders', 'Customers\Orders::index');
+    $routes->get('order/(:segment)', 'Customers\Orders::detail/$1');
+    $routes->get('checkout', 'Customers\Checkout::index');
+    $routes->get('profile', 'Customers\Profile::index');
+    $routes->get('profile/(:segment)/edit', 'Customers\Profile::edit/$1');
+});
+
+// Public page route
+$routes->get('about-us', 'Customer\Page::about');

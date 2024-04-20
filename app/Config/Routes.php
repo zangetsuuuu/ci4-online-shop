18897602251bc874e2fb54b215245b68a94701e2
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-20 21:49:59 +0700

    Update: New routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,32 +16,38 @@ $routes->get('/', 'Home::index');
 $routes->group('admin', function ($routes) {
     $routes->get('', 'Auth\Admin\Login::viewForm');
     $routes->get('create', 'Auth\Admin\Register::viewForm');
+    $routes->post('save', 'Auth\Admin\Register::saveAdminData');
     $routes->get('change-password', 'Auth\Password::admin');
     $routes->get('dashboard', 'Admin\Dashboard::index');
+    $routes->get('list', 'Admin\Admin::viewAdmins');
+    $routes->get('search', 'Admin\Admin::searchAdmin');
+    $routes->delete('(:num)/delete', 'Admin\Admin::deleteAdmin/$1');
 
     // Products
     $routes->get('products', 'Admin\Product::viewProducts');
-    $routes->get('product/search', 'Admin\Product::searchProduct');
-    $routes->get('product/create', 'Admin\Product::viewCreateProduct');
-    $routes->post('product/save', 'Admin\Product::saveProduct');
-    $routes->get('product/(:segment)', 'Admin\Product::viewProductDetail/$1');
-    $routes->get('product/(:segment)/edit', 'Admin\Product::viewEditProduct/$1');
-    $routes->post('product/(:num)/update', 'Admin\Product::updateProduct/$1');
-    $routes->delete('product/(:num)/delete', 'Admin\Product::deleteProduct/$1');
+    $routes->get('products/search', 'Admin\Product::searchProduct');
+    $routes->get('products/create', 'Admin\Product::viewCreateProduct');
+    $routes->post('products/save', 'Admin\Product::saveProduct');
+    $routes->get('products/(:segment)', 'Admin\Product::viewProductDetail/$1');
+    $routes->get('products/(:segment)/edit', 'Admin\Product::viewEditProduct/$1');
+    $routes->post('products/(:num)/update', 'Admin\Product::updateProduct/$1');
+    $routes->delete('products/(:num)/delete', 'Admin\Product::deleteProduct/$1');
 
     // Orders
     $routes->get('orders', 'Admin\Order::viewOrders');
-    $routes->get('order/(:segment)', 'Admin\Order::viewOrderDetail/$1');
+    $routes->get('orders/(:segment)', 'Admin\Order::viewOrderDetail/$1');
 
     // Customers
     $routes->get('customers', 'Admin\Customer::viewCustomers');
-    $routes->get('customer/search', 'Admin\Customer::searchCustomer');
-    $routes->get('customer/(:segment)', 'Admin\Customer::viewCustomerDetail/$1');
-    $routes->delete('customer/(:num)/delete', 'Admin\Customer::deleteCustomer/$1');
-
+    $routes->get('customers/search', 'Admin\Customer::searchCustomer');
+    $routes->get('customers/(:segment)', 'Admin\Customer::viewCustomerDetail/$1');
+    $routes->delete('customers/(:num)/delete', 'Admin\Customer::deleteCustomer/$1');
+    
     // Profile
-    $routes->get('profile', 'Admin\Profile::index');
+    $routes->get('profile', 'Admin\Profile::viewInfo');
     $routes->get('profile/(:segment)/edit', 'Admin\Profile::edit/$1');
+    
+    $routes->get('(:segment)', 'Admin\Admin::viewAdminDetail/$1');
 });
 
 // Customer routes

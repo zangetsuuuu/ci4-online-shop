6da29d6a17bc3639e21305363361f59f91b187fe
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-20 21:57:01 +0700

    Add: Admin model

---
+++ app/Models/Admin/AdminModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\Admin;
+
+use CodeIgniter\Model;
+
+class AdminModel extends Model
+{
+    protected $table = 'admins';
+    protected $returnType = 'array';
+    protected $useTimestamps = true;
+    protected $allowedFields = ['first_name', 'last_name', 'username', 'email', 'password', 'phone_number'];
+
+    public function getAdmins()
+    {
+        return $this->findAll();
+    }
+
+    public function getAdminByUsername($username)
+    {
+        $username = ltrim($username, '@');
+        return $this->where(['username' => $username])->first();
+    }
+
+    public function getAdminBySearch($keyword)
+    {
+        return $this->where("CONCAT(first_name, ' ', last_name, ' ', username, ' ', email, ' ', phone_number) LIKE '%$keyword%'", null, true)
+            ->findAll();
+    }
+}

7ba5466662f0f81fa3361c061b935f38b9046ec8
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-26 08:27:00 +0700

    Update: Add viewDashboard method

--- app/Controllers/Admin/Dashboard.php
+++ app/Controllers/Admin/Dashboard.php
@@ -4,12 +4,26 @@ namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
 
+use App\Models\Admin\ProductModel;
+use App\Models\Admin\CustomerModel;
+
 class Dashboard extends BaseController
 {
-    public function index(): string
+    protected $productModel;
+    protected $customerModel;
+
+    public function __construct()
+    {
+        $this->productModel = new ProductModel();
+        $this->customerModel = new CustomerModel();
+    }
+
+    public function viewDashboard()
     {
         $data = [
-            'title' => 'Dashboard | ADMIN'
+            'title' => 'Dashboard | ADMIN',
+            'totalProducts' => $this->productModel->getTotalProducts(),
+            'totalCustomers' => $this->customerModel->getTotalCustomers()
         ];
 
         return view('admin/dashboard', $data);

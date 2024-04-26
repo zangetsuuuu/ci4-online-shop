ee93f242fdb1dbf8d777fa23bf9e9960a8867c45
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-26 08:33:48 +0700

    Update: Change first_name and last_name to fullname

--- app/Models/Auth/Admin/RegisterModel.php
+++ app/Models/Auth/Admin/RegisterModel.php
@@ -9,5 +9,5 @@ class RegisterModel extends Model
     protected $table = 'admins';
     protected $returnType = 'array';
     protected $useTimestamps = true;
-    protected $allowedFields = ['first_name', 'last_name', 'username', 'email', 'password', 'phone_number'];
+    protected $allowedFields = ['fullname', 'username', 'email', 'password', 'phone_number'];
 }
--- app/Models/Auth/Customer/RegisterModel.php
+++ app/Models/Auth/Customer/RegisterModel.php
@@ -9,5 +9,5 @@ class RegisterModel extends Model
     protected $table = 'customers';
     protected $returnType = 'array';
     protected $useTimestamps = true;
-    protected $allowedFields = ['first_name', 'last_name', 'username', 'email', 'password', 'phone_number', 'address'];
+    protected $allowedFields = ['fullname', 'username', 'email', 'password', 'phone_number', 'address'];
 }

30c42a60151a3a79c9ead49a43582015296ce2bd
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-26 08:42:05 +0700

    Update

--- app/Views/layout/admin/customer/delete.php
+++ app/Views/layout/admin/customer/delete.php
@@ -7,7 +7,7 @@
                     <svg class="mx-auto w-24 h-24 mb-4 text-red-700" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M12 16.99V17M12 7V14M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
-                    <h3 class="mb-5 text-base font-normal text-gray-500 tracking-wide">Hapus pelanggan "<?= $data['first_name'] . ' ' . $data['last_name']; ?>"?</h3>
+                    <h3 class="mb-5 text-base font-normal text-gray-500 tracking-wide">Hapus pelanggan "<?= $data['fullname']; ?>"?</h3>
                     <div class="flex items-center justify-center space-x-3">
                         <form class="d-inline" action="<?= base_url("admin/customer/$data[id]/delete"); ?>" method="post">
                             <?= csrf_field(); ?>

56894d4d4617f465e4733fe6678e4ce2d5be2a4a
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-27 17:21:08 +0700

    Update: Minor changes

--- app/Controllers/Customer/Product.php
+++ app/Controllers/Customer/Product.php
@@ -4,21 +4,25 @@ namespace App\Controllers\Customer;
 
 use App\Controllers\BaseController;
 use App\Models\Customer\ProductModel;
+use App\Models\Customer\CartModel;
 
 class Product extends BaseController
 {
     protected $productModel;
+    protected $cartModel;
 
     public function __construct()
     {
         $this->productModel = new ProductModel();
+        $this->cartModel = new CartModel();
     }
 
     public function viewProducts()
     {
         $data = [
             'title' => 'Daftar Produk',
-            'products' => $this->productModel->getProducts()
+            'products' => $this->productModel->getProducts(),
+            'validation' => session()->getFlashdata('validation')
         ];
 
         return view('customer/product/index', $data);
@@ -36,5 +40,52 @@ class Product extends BaseController
         }
 
         return view('customer/product/detail', $data);
+    }
+
+    public function addToCart()
+    {
+        $config = [
+            'quantity' => [
+                'rules' => 'required|integer|less_than[stock]',
+                'errors' => [
+                    'required' => 'Jumlah produk tidak boleh kosong!',
+                    'integer' => 'Jumlah produk harus berupa bilangan bulat!',
+                    'less_than' => 'Jumlah pesanan tidak boleh lebih dari stok!'
+                ]
+            ]
+        ];
+
+        if (!$this->validate($config)) {
+            return redirect()->back()->withInput()->with('validation', $this->validator);
+        }
+
+        $productId = $this->request->getVar('product_id');
+        $quantity  = $this->request->getVar('quantity');
+
+        $product = $this->productModel->find($productId);
+
+        $cartItem = $this->cartModel->where('product_id', $productId)
+            ->where('customer_id', 1)
+            ->first();
+
+        if ($cartItem) {
+            $this->cartModel->update($cartItem['id'], ['quantity' => $cartItem['quantity'] + $quantity]);
+        } else {
+            $data = [
+                'customer_id' => 1,
+                'product_id' => $productId,
+                'quantity' => $quantity,
+                'price' => $product['price']
+            ];
+
+            $this->cartModel->save($data);
+        }
+
+        $newStock = $product['stock'] - $quantity;
+        $this->productModel->update($productId, ['stock' => $newStock]);
+
+        session()->setFlashdata('Add Success', 'Produk ditambahkan ke keranjang!');
+
+        return redirect()->to('products');
     }
 }
